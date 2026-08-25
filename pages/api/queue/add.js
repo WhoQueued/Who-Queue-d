b36d0d292742a9aae4c2721ca37c238b2e782abd
WhoQueued <jordisweet@hotmail.co.uk>
2026-08-25 14:56:51 +0400

    Update add.js

--- pages/api/queue/add.js
+++ pages/api/queue/add.js
@@ -29,7 +29,6 @@ export default async function handler(req, res) {
     if (!playlistId) {
       playlistId = await getOrCreatePlaylist({
         accessToken: session.accessToken,
-        userId: session.userId,
         playlistName: "Who Queue'd?",
       });
       appendCookie(
